58603581e2abad2b89f2385ba72b413868c3513e
Murtaza Udaipurwala <murtaza.u6e24@gmail.com>
2022-01-30 18:15:06 +0530

    utility method to check if timer is running

--- pom.go
+++ pom.go
@@ -1,5 +1,20 @@
 package main
 
+import "time"
+
+func isRunning(s State) (time.Time, bool) {
+	if s.Running != true || len(s.EndsAt) == 0 {
+		return time.Time{}, false
+	}
+
+	endt, err := time.Parse(time.RFC3339, s.EndsAt)
+	if err != nil {
+		return time.Time{}, false
+	}
+
+	return endt, true
+}
+
 func start() {
 
 }
