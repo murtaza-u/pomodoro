ce626635ae5aba2a7a83fb85ddc0ac7984b40e4e
Murtaza Udaipurwala <murtaza.u6e24@gmail.com>
2022-01-30 18:08:02 +0530

    reading state file

--- state.go
+++ state.go
@@ -1,6 +1,26 @@
 package main
 
+import (
+	"encoding/json"
+	"io/ioutil"
+)
+
 type State struct {
 	Running string `json:"running"`
 	Ends    string `json:"ends"`
+}
+
+const path = "/tmp/pomodoro.json"
+
+func (s *State) read() error {
+	data, err := ioutil.ReadFile(path)
+	if err != nil {
+		return err
+	}
+
+	return json.Unmarshal(data, s)
+}
+
+func (s *State) write() error {
+
 }
