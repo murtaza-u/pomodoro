e3ae8eb4e4a06754a2bd8d3632154b5f032ff9d3
Murtaza Udaipurwala <murtaza.u6e24@gmail.com>
2022-03-07 00:16:52 +0530

    updated help

--- cmd.go
+++ cmd.go
@@ -26,7 +26,9 @@ add [duration(in min)]
   - Takes duration(in minutes) as a compulsory argument
 
 help
-  - Displays this help information%s`,
+  - Displays this help information
+
+Add 'complete -C pom pom' in your .bashrc to enable completion%s`,
 		os.Args[0],
 		os.Args[0],
 		"\n",
